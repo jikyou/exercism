cd2e86856a196aa149f5c85032b36910b458ba1b
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-17 20:25:47 +0900

    Refactory ruby matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -10,13 +10,7 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(matrix)
-    @rows = matrix.split("\n").map { |row| row.split(' ').map(&:to_i) }
-    @columns = @rows.reduce([]) do |acc, cur|
-      cur.each_with_index do |ele, index|
-        acc[index] = [] if acc[index].nil?
-        acc[index].push(ele)
-      end
-      acc
-    end
+    @rows = matrix.each_line.map { |row| row.split.map(&:to_i) }
+    @columns = @rows.transpose
   end
 end
