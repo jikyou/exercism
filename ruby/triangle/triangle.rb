b29c948d48aacd88ba8e96eea29320373c3408e8
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-28 13:37:25 +0900

    Add ruby exercise triangle

---
+++ ruby/triangle/triangle.rb
@@ -0,0 +1,42 @@
+=begin
+Write your code for the 'Triangle' exercise in this file. Make the tests in
+`triangle_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/triangle` directory.
+=end
+
+class Triangle
+  def initialize(sides)
+    @sides = sides
+  end
+
+  def equilateral?
+    return false unless istriangle?
+
+    @sides[0] == @sides[1] && @sides[0] == @sides[2]
+  end
+
+  def isosceles?
+    return false unless istriangle?
+
+    @sides[0] == @sides[1] || @sides[0] == @sides[2] || @sides[1] == @sides[2]
+  end
+
+  def scalene?
+    return false unless istriangle?
+
+    @sides[0] != @sides[1] && @sides[0] != @sides[2] && @sides[1] != @sides[2]
+  end
+
+  private
+
+  def istriangle?
+    return false if @sides.any?(0)
+    return false if @sides[0] + @sides[1] < @sides[2]
+    return false if @sides[0] + @sides[2] < @sides[1]
+    return false if @sides[1] + @sides[2] < @sides[0]
+
+    true
+  end
+end
