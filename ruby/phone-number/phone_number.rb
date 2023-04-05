5c37f4f53c5837f1894be66c41c900bd400a8bdd
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-05 09:59:16 +0900

    Add ruby exercise phone-number

---
+++ ruby/phone-number/phone_number.rb
@@ -0,0 +1,18 @@
+=begin
+Write your code for the 'Phone Number' exercise in this file. Make the tests in
+`phone_number_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/phone-number` directory.
+=end
+
+class PhoneNumber
+  def self.clean(phone)
+    phone = phone.gsub(/\D/, '')
+    phone = phone[1..] if phone.size == 11 && phone[0].to_i == 1
+    return if phone.size != 10
+    return if phone[0].to_i < 2 || phone[3].to_i < 2
+
+    phone
+  end
+end
