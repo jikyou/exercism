1e8eeaf16fed8cce183aaeaed6bec9539b80174c
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-07 12:05:53 +0900

    Add ruby exercise saddle-points

---
+++ ruby/saddle-points/saddle_points.rb
@@ -0,0 +1,29 @@
+=begin
+Write your code for the 'Saddle Points' exercise in this file. Make the tests in
+`saddle_points_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/saddle-points` directory.
+=end
+
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(str)
+    @rows = str.split("\n").map { |e| e.split.map(&:to_i) }
+    @columns = @rows.each_with_object([]) do |row, acc|
+      row.each_with_index do |item, index|
+        acc[index] = [] if acc[index].nil?
+        acc[index].push(item)
+      end
+    end
+  end
+
+  def saddle_points
+    @rows.each_with_index.each_with_object([]) do |(item, y), acc|
+      item.each_index do |x|
+        acc.push([y, x]) if item[x] == item.max && item.max == @columns[x].min
+      end
+    end
+  end
+end
