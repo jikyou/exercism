3bc8d36b44f8cced6388bf8640f1fb58fa53b207
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-10 23:28:22 +0900

    Update etl use flat_map and product

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -6,10 +6,20 @@ To get started with TDD, see the `README.md` file in your
 `ruby/etl` directory.
 =end
 
-class ETL
+class ETL1
   def self.transform(old)
     old.each_with_object({}) do |(score, letters), a|
       letters.map(&:downcase).each { |e| a[e] = score }
+    end
+  end
+end
+
+class ETL
+  class << self
+    def transform(legacy)
+      Hash[legacy.flat_map do |score, letters|
+        letters.map(&:downcase).product([score])
+      end]
     end
   end
 end
