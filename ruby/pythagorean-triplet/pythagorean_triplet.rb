63c4e5807a0dc71f8bb19c96bd44e7c9afbde176
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-11 22:58:41 +0900

    Update O(n) method

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -6,7 +6,7 @@ To get started with TDD, see the `README.md` file in your
 `ruby/pythagorean-triplet` directory.
 =end
 
-class PythagoreanTriplet
+class PythagoreanTriplet1
   def self.triplets_with_sum(num)
     a_start = num / 3
     (1..a_start).each_with_object([]) do |a, acc|
@@ -18,5 +18,19 @@ class PythagoreanTriplet
         acc << [a, b, c] if a**2 + b**2 == c**2
       end
     end
+  end
+end
+
+class PythagoreanTriplet
+  def self.triplets_with_sum(sum)
+    a_limit = (sum / 3)
+    (1..a_limit)
+      .map do |a|
+        b = (sum**2 - 2 * sum * a) / (2 * sum - 2 * a)
+        c = sum - (a + b)
+        [a, b, c]
+      end
+      .select { |a, b, c| a < b && b < c }
+      .select { |a, b, c| a**2 + b**2 == c**2 }
   end
 end
