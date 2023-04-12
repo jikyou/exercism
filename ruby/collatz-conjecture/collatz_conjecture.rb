96471dcd8de6a4d9b71b9a36b864fde1bde4e40a
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-12 15:49:03 +0900

    Add ruby exercise collatz-conjecture

---
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -0,0 +1,22 @@
+=begin
+Write your code for the 'Collatz Conjecture' exercise in this file. Make the tests in
+`collatz_conjecture_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/collatz-conjecture` directory.
+=end
+
+class CollatzConjecture
+  def self.steps(num)
+    raise ArgumentError if num <= 0
+
+    step = 0
+    loop do
+      break if num == 1
+
+      num = num.even? ? num / 2 : num * 3 + 1
+      step += 1
+    end
+    step
+  end
+end
