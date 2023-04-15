137cc4b5db1552e42062e4dd892cc47392754bf0
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-15 13:53:36 +0900

    Add ruby exercise proverb

---
+++ ruby/proverb/proverb.rb
@@ -0,0 +1,22 @@
+=begin
+Write your code for the 'Proverb' exercise in this file. Make the tests in
+`proverb_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/proverb` directory.
+=end
+
+class Proverb
+  def initialize(*words, **opt)
+    @words = words
+    @qualifier = opt[:qualifier] ? "#{opt[:qualifier]} #{words[0]}" : words[0]
+  end
+
+  def to_s
+    @words.each_index.map do |i|
+      next "And all for the want of a #{@qualifier}." if i == @words.size - 1
+
+      "For want of a #{@words[i]} the #{@words[i + 1]} was lost."
+    end.join("\n")
+  end
+end
