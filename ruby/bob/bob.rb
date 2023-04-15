15b07508922d40d0a1e1654487fca32f483f648f
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-15 16:18:58 +0900

    Update use @say variable

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -8,24 +8,24 @@ To get started with TDD, see the `README.md` file in your
 
 class Bob
   def self.hey(say)
-    say = say.strip
-    return "Calm down, I know what I'm doing!" if question?(say) && shouting?(say)
-    return 'Sure.' if question?(say)
-    return 'Whoa, chill out!' if shouting?(say)
-    return 'Fine. Be that way!' if silent?(say)
+    @say = say.strip
+    return "Calm down, I know what I'm doing!" if question? && shouting?
+    return 'Sure.' if question?
+    return 'Whoa, chill out!' if shouting?
+    return 'Fine. Be that way!' if silent?
 
     'Whatever.'
   end
 
-  def self.silent?(say)
-    say.empty?
+  def self.silent?
+    @say.empty?
   end
 
-  def self.question?(say)
-    say.end_with?('?')
+  def self.question?
+    @say.end_with?('?')
   end
 
-  def self.shouting?(say)
-    !say.scan(/[a-zA-Z]+/).empty? && say == say.upcase
+  def self.shouting?
+    !@say.scan(/[a-zA-Z]+/).empty? && @say == @say.upcase
   end
 end
