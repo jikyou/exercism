8549e80886836cdb91f9adf88f02f58aaf319d37
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-15 15:40:17 +0900

    Update all_capital_letters change to shouting?

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -9,9 +9,9 @@ To get started with TDD, see the `README.md` file in your
 class Bob
   def self.hey(say)
     say = say.strip
-    return "Calm down, I know what I'm doing!" if question(say) && all_capital_letters(say)
+    return "Calm down, I know what I'm doing!" if question(say) && shouting?(say)
     return 'Sure.' if question(say)
-    return 'Whoa, chill out!' if all_capital_letters(say)
+    return 'Whoa, chill out!' if shouting?(say)
     return 'Fine. Be that way!' if say.gsub(/\s/, '').empty?
 
     'Whatever.'
@@ -21,7 +21,7 @@ class Bob
     say.end_with?('?')
   end
 
-  def self.all_capital_letters(say)
+  def self.shouting?(say)
     !say.scan(/[a-zA-Z]+/).empty? && say == say.upcase
   end
 end
