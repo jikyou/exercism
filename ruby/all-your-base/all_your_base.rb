ca7a3f82bf81a78ab30b06c22e317ae5ad3a77dd
ChenZiQiang <ziqiang.chen@outlook.com>
2023-04-19 18:37:10 +0900

    Update all-your-base rewrite

--- ruby/all-your-base/all_your_base.rb
+++ ruby/all-your-base/all_your_base.rb
@@ -6,48 +6,16 @@ To get started with TDD, see the `README.md` file in your
 `ruby/all-your-base` directory.
 =end
 class BaseConverter
-  class << self
-    def convert(input_base, digits, output_base)
-      raise ArgumentError if input_base < 2 || output_base < 2
-      raise ArgumentError if digits.any? { |i| i.negative? || i >= input_base }
+  def self.convert(input_base, digits, output_base)
+    raise ArgumentError if input_base < 2 || output_base < 2
+    raise ArgumentError if digits.any? { |i| i.negative? || i >= input_base }
 
-      @input_base = input_base
-      @output_base = output_base
-
-      sum = sum(digits)
-      output_digits(sum)
+    sum = digits.reduce(0) { |s, d| s * input_base + d }
+    to_digits = []
+    while sum.positive?
+      sum, rem = sum.divmod(output_base)
+      to_digits.unshift(rem)
     end
-
-    private
-
-    attr_reader :input_base, :output_base
-
-    def output_digits(sum)
-      first = first_pos(sum)
-      digits = []
-      while first >= 0
-        base = output_base**first
-        digit = sum / base
-        digits << digit
-
-        sum -= digit * base
-        first -= 1
-      end
-      digits
-    end
-
-    def first_pos(sum)
-      first = 0
-      first += 1 while output_base**(first + 1) < sum
-      first
-    end
-
-    def sum(digits)
-      digits
-        .reverse
-        .each_with_index
-        .map { |item, index| input_base**index * item }
-        .sum
-    end
+    to_digits.empty? ? [0] : to_digits
   end
 end
