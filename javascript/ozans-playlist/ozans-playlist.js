1735d98b9e101b95d37c63008bcdf2e9815b3334
ChenZiQiang <ziqiang.chen@outlook.com>
2023-05-03 09:48:40 +0900

    Add javascript exercise

---
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -0,0 +1,63 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(playlist) {
+  return [...new Set(playlist)]
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  return playlist.includes(track)
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  let list = new Set(playlist)
+  list.add(track)
+  return [...list]
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+  let list = new Set(playlist)
+  list.delete(track)
+  return [...list]
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+  let artists = playlist.map(r => r.split(/\s-\s/)[1])
+  return [...new Set(artists)]
+}
