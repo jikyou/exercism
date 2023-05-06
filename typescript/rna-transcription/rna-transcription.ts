5337496b70f4e6ca89f02f9e985f002d758014f7
ChenZiQiang <ziqiang.chen@outlook.com>
2023-05-06 09:56:05 +0900

    Update use reduce to repalce replaceAll

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,11 +1,16 @@
+const DNATORNAPAIR: { [key: string]: string } = {
+  "C": "G",
+  "G": "C",
+  "A": "U",
+  "T": "A",
+}
+
 export function toRna(DNAStrand: string) {
   if (/[^CGAT]/.test(DNAStrand)) {
     throw new Error("Invalid input DNA.")
   }
-  return DNAStrand
-    .replaceAll("C", "X")
-    .replaceAll("G", "C")
-    .replaceAll("X", "G")
-    .replaceAll("A", "U")
-    .replaceAll("T", "A")
+  return [...DNAStrand].reduce((acc, cur) => {
+    acc += DNATORNAPAIR[cur]
+    return acc
+  }, "")
 }
