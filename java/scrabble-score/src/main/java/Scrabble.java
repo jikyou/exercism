c318a81e011e16e7f72bb656bdd7aa20cd9be7a5
ChenZiQiang <ziqiang.chen@outlook.com>
2023-09-06 23:33:01 +0900

    Update simplify put operate code

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,38 +1,20 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 class Scrabble {
-    private static Map<String, Integer> LETTER_SCORE = new HashMap<>() {
-        {
-            put("A", 1);
-            put("E", 1);
-            put("I", 1);
-            put("O", 1);
-            put("U", 1);
-            put("L", 1);
-            put("N", 1);
-            put("R", 1);
-            put("S", 1);
-            put("T", 1);
-            put("D", 2);
-            put("G", 2);
-            put("B", 3);
-            put("C", 3);
-            put("M", 3);
-            put("P", 3);
-            put("F", 4);
-            put("H", 4);
-            put("V", 4);
-            put("W", 4);
-            put("Y", 4);
-            put("K", 5);
-            put("J", 8);
-            put("X", 8);
-            put("Q", 10);
-            put("Z", 10);
-        }
-    };
+    private static Map<String, Integer> LETTER_SCORE = new HashMap<>(26);
     private String word;
+
+    static {
+        Arrays.stream("AEIOULNRST".split("")).forEach(c -> LETTER_SCORE.put(c, 1));
+        Arrays.stream("DG".split("")).forEach(c -> LETTER_SCORE.put(c, 2));
+        Arrays.stream("BCMP".split("")).forEach(c -> LETTER_SCORE.put(c, 3));
+        Arrays.stream("FHVWY".split("")).forEach(c -> LETTER_SCORE.put(c, 4));
+        Arrays.stream("K".split("")).forEach(c -> LETTER_SCORE.put(c, 5));
+        Arrays.stream("JX".split("")).forEach(c -> LETTER_SCORE.put(c, 8));
+        Arrays.stream("QZ".split("")).forEach(c -> LETTER_SCORE.put(c, 10));
+    }
 
     Scrabble(String word) {
         this.word = word.toUpperCase();
