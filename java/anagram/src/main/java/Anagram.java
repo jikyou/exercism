1c2363a388639d1a538501bc161f6263f63da85b
ChenZiQiang <ziqiang.chen@outlook.com>
2023-09-10 18:21:56 +0900

    Refactoring use small function

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,29 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 class Anagram {
     private String word;
 
     public Anagram(String word) {
-        this.word = word.toLowerCase();
+        this.word = word;
     }
 
     public List<String> match(List<String> candidates) {
-        var wordChars = this.word.toCharArray();
-        Arrays.sort(wordChars);
-        return candidates.stream().filter(word -> {
-            word = word.toLowerCase();
-            if (word.equals(this.word)) {
-                return false;
-            }
+        var sortedWord = sort(this.word);
+        return candidates.stream()
+                .filter(word -> !word.equalsIgnoreCase(this.word))
+                .filter(word -> word.length() == this.word.length())
+                .filter(word -> sort(word).equals(sortedWord))
+                .collect(Collectors.toList());
+    }
 
-            var tempChars = word.toCharArray();
-            Arrays.sort(tempChars);
-            return new String(tempChars).equals(new String(wordChars));
-        }).collect(Collectors.toList());
+    private String sort(String word) {
+        return word.toLowerCase()
+                .chars()
+                .mapToObj(c -> String.valueOf(c))
+                .sorted()
+                .collect(Collectors.joining());
     }
 
 }
