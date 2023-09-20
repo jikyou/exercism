bda7d075e8fe45fa60a930d1ce3ec642c39e8603
ChenZiQiang <ziqiang.chen@outlook.com>
2023-09-20 18:26:11 +0900

    Update remove unless ignore reference

--- java/resistor-color/src/test/java/ResistorColorTest.java
+++ java/resistor-color/src/test/java/ResistorColorTest.java
@@ -1,9 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Arrays;
 
 public class ResistorColorTest {
 
